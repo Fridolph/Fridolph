23aae3ce941db0263e0b53256c5ea60fbdf373f9
fridolph <249121486@qq.com>
2016-10-27 10:32:31 +0800

    创建正则~ 正则的一些使用

--- Javascript忍者秘籍/js/assert.js
+++ Javascript忍者秘籍/js/assert.js
@@ -1,7 +1,10 @@
 function assert(value, desc) {
+  
   var doc = document,
       li = document.createElement('li');
+  
   li.className = value ? "pass" : "fail";
   li.appendChild(doc.createTextNode(desc));
   doc.getElementById('results').appendChild(li);
+
 }

0f1505a879ef15023834b4671008b700c8553e2e
fridolph <249121486@qq.com>
2016-10-31 13:31:12 +0800

    修改了断言函数 assert 这次不用添加style 和 <ul>的标签了~ 一切都动态地生成出来

--- Javascript忍者秘籍/js/assert.js
+++ Javascript忍者秘籍/js/assert.js
@@ -1,10 +1,25 @@
 function assert(value, desc) {
-  
   var doc = document,
-      li = document.createElement('li');
-  
+      ul = document.createElement("ul"),
+      li = document.createElement("li");
+
+  if( !doc.getElementById("results") ) {
+    doc.getElementsByTagName("body")[0].appendChild(ul);
+    ul.id = "results";
+  }
+
   li.className = value ? "pass" : "fail";
   li.appendChild(doc.createTextNode(desc));
-  doc.getElementById('results').appendChild(li);
+  
 
+  var pass = doc.getElementById("results").getElementsByClassName("pass");
+  var fail = doc.getElementById("results").getElementsByClassName("fail");
+  doc.getElementById("results").appendChild(li);
+
+  for( var i=0; i<pass.length; i++ ) {
+    pass[i].style.cssText = 'color: green;padding: 5px; border:1px solid #ccc;';
+  }
+  for( var i=0; i<fail.length; i++ ) {
+    fail[i].style.cssText = 'color: red; text-decoration: line-through;padding: 5px; border:1px solid #ccc;';
+  }
 }

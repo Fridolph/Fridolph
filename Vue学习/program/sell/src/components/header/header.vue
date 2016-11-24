a299936be895654c587819a01e85b923835c0c5f
fridolph <249121486@qq.com>
2016-11-24 21:36:18 +0800

    头部组件 完成。。开始开发 头部内部组件~  弹出层联动效果

--- Vue学习/program/sell/src/components/header/header.vue
+++ Vue学习/program/sell/src/components/header/header.vue
@@ -13,12 +13,22 @@
           {{seller.description}} / {{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <!-- <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span> -->
-          <!-- <span class="text">{{seller.supports[0].description}}</span> -->
+          <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="header-bg">
+      <img :src="seller.avatar" width="100%" height="100%" />
+    </div>
   </div>
 </template>
 
@@ -30,7 +40,7 @@
       }
     },
     created() {
-      this.classMap = ['decrease', 'discount', 'guarantee', 'special', 'guarantee']
+      this.classMap = ['decreaase', 'discount', 'special', 'invoice', 'guarantee']
     }
   }
 </script>
@@ -39,9 +49,11 @@
   @import "../../common/stylus/mixin"
 
   .header
+    position: relative
     color: #fff
-    background-color: #333
+    background-color: rgba(7, 17, 27, 0.5)
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size:0
       .avatar
@@ -68,21 +80,80 @@
             font-size: 16px
             line-height: 18px
             font-weight: bold
-        .desription
+        .description
           margin-bottom: 10px
           line-height: 12px
           font-size: 12px
         .support
           .icon
             display: inline-block
-            vertical-align: top
             width: 12px
             height: 12px
             margin-right: 4px
             background-size: 12px 12px
-            background-repeat: no-repeat
-            
+            background-reapeat: no-repeat
+            &.decreaase
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.special
+              bg-image('special_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.guarantee
+              bg-image('guarantee_1')
           .text
             line-height: 12px
-            font-size: 12px
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0,0,0,0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          line-height: 24px
+          margin-left: 2px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative      
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background-color: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px 
+        top: 10px
+    .header-bg
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>

c224ad70bbe883903b735ef8498fc9d9b8041270
fridolph <249121486@qq.com>
2016-11-24 20:51:11 +0800

    header 未完成~~ 差图标（选择class组问题）

--- Vue学习/program/sell/src/components/header/header.vue
+++ Vue学习/program/sell/src/components/header/header.vue
@@ -12,6 +12,10 @@
         <div class="description">
           {{seller.description}} / {{seller.deliveryTime}}分钟送达
         </div>
+        <div v-if="seller.supports" class="support">
+          <!-- <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span> -->
+          <!-- <span class="text">{{seller.supports[0].description}}</span> -->
+        </div>
       </div>
     </div>
     <div class="bulletin-wrapper"></div>
@@ -24,10 +28,61 @@
       seller: {
         type: Object
       }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'guarantee', 'special', 'guarantee']
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  
+  @import "../../common/stylus/mixin"
+
+  .header
+    color: #fff
+    background-color: #333
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      font-size:0
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: 14px
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            vertical-align: top
+            display: inline-block
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
+        .desription
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            
+          .text
+            line-height: 12px
+            font-size: 12px
 </style>

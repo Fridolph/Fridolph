d2bbd74e8a73df78b8295014f28a3e6314229bc2
fridolph <249121486@qq.com>
2016-11-24 09:17:38 +0800

    vue项目实战~~ 外卖APP~SPA

---
+++ Vue学习/program/sell/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header'
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ Vue学习/program/sell/src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    我的header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  
+</style>

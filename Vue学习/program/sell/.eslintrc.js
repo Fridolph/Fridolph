556ae8e1595fda9270d3b68d2803e03629317619
fridolph <249121486@qq.com>
2017-01-13 09:22:32 +0800

    eslintrc配置

--- Vue学习/program/sell/.eslintrc.js
+++ Vue学习/program/sell/.eslintrc.js
@@ -21,7 +21,10 @@ module.exports = {
     // 'indent': '0',   默认缩进0
     // 这条规则是说 function abc() {}   abc后与括号间必须有一格空格，我们去掉~~
     'space-before-function-paren': 0,
-    // 
-    "comma-dangle": 0
+    // a,
+    // b, 末尾写逗号不报错
+    'comma-dangle': 0,
+    // 定义没使用的变量不报错
+    'no-unused-vars': 0,
   }
 }

f75b342410439b054b28df0eb60d3ea0561e2328
fridolph <249121486@qq.com>
2017-01-13 20:13:37 +0800

    sell项目~~ seller模块

--- Vue学习/program/sell/src/components/seller/seller.vue
+++ Vue学习/program/sell/src/components/seller/seller.vue
@@ -1,11 +1,141 @@
 <template>
-  <div>I am seller</div>
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{ seller.name }}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{ seller.ratingCount }})</span>
+          <span class="text">月售{{ seller.sellCount }}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2 class="title">起送价</h2>
+            <div class="content">
+              <span class="stress">{{ seller.minPrice }}</span>元              
+            </div>
+          </li>
+          <li class="block">
+            <h2 class="title">商家配送</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryPrice }}</span>元              
+            </div>
+          </li>
+          <li class="block">
+            <h2 class="title">平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryTime }}</span>分钟              
+            </div>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{ seller.bulletin }}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="item in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+            <span class="text">{{seller.supports[$index].description}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="ecmascript-6">
-  export default {}
+  import star from 'components/star/star'
+  import split from 'components/common/split/split'
+
+  export default {
+    props: {
+      seller: {
+        type: Object,        
+      }
+    },
+    created() {
+      this.classMap = ['decreaase', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    components: {
+      star,
+      split,
+    },    
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixin.styl'
   
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px        
+        border-1px(rgba(7,17,27,0.1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-right: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          margin-right: 12px
+          vertical-align: top
+          font-size: 10px
+          line-height: 18px
+          color: rgb(77,85,93)
+      .remark
+        display: flex
+        padding-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7,17,27,0.1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 10px
+            font-size: 10px
+            line-height: 10px
+            color: rgb(147,153,159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7,17,27)
+            .stress
+              font-size: 24px
+    .bulletin
+      padding: 18px 18px 0 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rgba(7,17,27,0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240,20,20)
+      .supports
+        .supports-item
+          padding: 16px 12px
+          font-size: 0
+          border-1px(rgba(7,17,27,0.1))
+          
 </style>

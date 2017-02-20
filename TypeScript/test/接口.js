57e99e3875f8df6ed1ee66162f2a54c45d7925ea
fridolph <249121486@qq.com>
2017-02-20 16:47:17 +0800

    ts

---
+++ TypeScript/test/接口.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = { firstName: "Jane", lastName: "User" };
+document.body.innerHTML = greeter(user);

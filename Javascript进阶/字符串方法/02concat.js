a698b767c5da85afeab04b0356b250ed9eb96f1c
fridolph <249121486@qq.com>
2017-05-02 14:00:30 +0800

    常用的数组，字符串方法

---
+++ Javascript进阶/字符串方法/02concat.js
@@ -0,0 +1,5 @@
+// concat 方法将一个或多个字符串与原字符串连接合并，形成一个新的字符串并返回。 concat 方法并不影响原字符串。
+// str.concat(string2, string3[, ..., stringN])
+
+var hello = "Hello, ";
+console.log(hello.concat('Kevin', ' have a nice day.')); /* Hello, Kevin have a nice day. */
---
+++ Javascript进阶/数组方法/02from.js
@@ -0,0 +1,8 @@
+// Array.from() 方法从一个类似数组或可迭代对象创建一个新的数组实例。
+
+const bar = ["a", "b", "c"];
+console.log(Array.from(bar));
+// ["a", "b", "c"]
+
+console.log(Array.from('foo'));
+// ["f", "o", "o"]

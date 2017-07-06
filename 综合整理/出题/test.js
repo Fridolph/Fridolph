5d395efe8fbdab2d08405bbc4f70f902a915d007
fridolph <249121486@qq.com>
2017-07-06 18:05:41 +0800

    出题整理~

--- 综合整理/出题/test.js
+++ 综合整理/出题/test.js
@@ -1,10 +1,23 @@
-function paixu() {
+// // 判断一个字符串中出现次数最多的字符，统计这个次数
+let str = 'asdfssaaasasasasaa';
 
-  console.log(typeof arguments);
+function showStringCount(str = '') {  
+  let result = {};
+  let strArr = str.split('');
 
-  if (arguments != null) {
-    console.log('参数是number');
+  if (str != null) {
+    strArr.map((s, i) => {
+      // console.log(`s: ${s}, i: ${i}`);
+      if (!result[s]) {
+        result[s] = 1;
+      } else (result[s] != null) {
+        result[s]++
+      }
+    });
+
+    return result
+
+  } else {
+    return '请传入合法字符串'
   }
-  
-  return '传入的类型不对';  
 }

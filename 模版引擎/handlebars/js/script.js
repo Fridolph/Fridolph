665805333b0035010fd424ce4e4f96d1555c6961
fridolph <249121486@qq.com>
2017-07-25 21:13:05 +0800

    handlebars学习~~ 实战小demo

--- 模版引擎/handlebars/js/script.js
+++ 模版引擎/handlebars/js/script.js
@@ -2,6 +2,13 @@
 
   var GETCLASSES = 'http://imoocnote.calfnote.com/inter/getClasses.php'
 
+  // 添加全局的错误处理
+  $.ajaxSetup({
+    error: function() {
+      alert('调用接口失败');
+    }
+  })
+
   $.getJSON(GETCLASSES, {curPage: 1}, function(data) {
     console.log(data)
 
@@ -10,6 +17,22 @@
     var h = f(data.data);
 
     $('#classes').html(h);
+  })
+
+  Handlebars.registerHelper('equal', function(v1, v2, options) {
+    if (v1 == v2) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  })
+
+  Handlebars.registerHelper('long', function(value, options) {
+    if (value.indexOf('小时') != -1) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
   })
 
 })(jQuery)

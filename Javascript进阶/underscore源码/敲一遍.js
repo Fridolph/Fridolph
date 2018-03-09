115b9fa6edcd908fd30a63ac67c4d1c47d401c1b
fridolph <249121486@qq.com>
2018-03-09 15:59:58 +0800

    资料整理 underscore源码， 面试，wepback

--- Javascript进阶/underscore源码/敲一遍.js
+++ Javascript进阶/underscore源码/敲一遍.js
@@ -803,13 +803,154 @@
     }
   })
 
+  // Memoize on expensive function by storing its results
+  _.memoize = function(func, hasher) {
+    var memoize = function(key) {
+      var cache = memoize.cache;
+      var address = '' + (hasher ? hasher.apply(this, arguments) : key)
+      if (!_.has(cache, address)) cache[address] = func.apply(this, arguments)
+      return cache[address]
+    }
+    memoize.cache = {}
+    return memoize
+  }
+
+  // Delays a function for the given number of milliseconds and then calls
+  // it with the auguments supplied.
+  _.delay = restArgs(function(func, wait, args) {
+    return setTimeout(function() {
+      return func.apply(null, args)
+    }, wait)
+  })
+
+  // Defera a function, scheduling it to run after the current call stack has cleared
+  _.defer = _.partial(_.delay, _, 1)
+
+  // Returns a function, that ,when invoked, will only be triggered at most once
+  // during a given window of time, Normally, the throttled function will run 
+  // as much as it can, without ever going more than once per 'wait' duration
+  // but if you'd like to disable the execution on the leading edge, pass 
+  // `{leading: false}`. To disable execution on the trailing edge, ditto.
+  _.throttle = function(func, wait, options) {
+    var timeout, context, args, result;
+    var previous = 0;
+    if (!options) options = {}
+
+    var later = function() {
+      previous = options.leading === false ? 0 : _.now()
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null
+    }
+
+    var throttled = function() {
+      var now = _.now()
+      if (!previous && options.leading === false) previous = now
+      var remaining = wait - (now - previous)
+      context = this
+      args = arguments
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout)
+          timeout = null
+        }
+        previous = now
+        result = func.apply(context, args)
+        if (!timeout) context = args = null
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining)
+      } 
+      return result
+    }
+
+    throttled.cancel = function() {
+      clearTimeout(timeout)
+      previous = 0
+      timeout = context = args = null
+    }
   
+    return throttled
+  }
 
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  _.debounce = function(func, wait, immediate) {
+    var timeout, result
 
+    var later = function(context, args) {
+      timeout = null;
+      if (args) result = func.apply(context, args)
+    }
 
+    var debounced = testArgs(function(args) {
+      if (timeout) clearTimeout(timeout)
+      if (immediate) {
+        var callNow = !timeout
+        timeout = setTimeout(later, wait)
+        if (callNow) result = func.apply(this, args)
+      } else {
+        timeout = _.delay(later, wait, this, args)
+      }
 
+      return result
+    })
 
+    debounced.cancel = function() {
+      clearTimeout(timeout)
+      timeout = null
+    }
 
+    return debounced
+  }
 
+  // Returns the first function passed as an argument to the second,
+  // allowing you to adjust arguments, run code before and after, and
+  // conditionally execute the original function.
+  _.wrap = function(func, wrapper) {
+    return _.partial(wrapper, func);
+  };
+
+  // Returns a negated version of the passed-in predicate.
+  _.negate = function(predicate) {
+    return function() {
+      return !predicate.apply(this, arguments);
+    };
+  };
+
+  // Returns a function that is the composition of a list of functions, each
+  // consuming the return value of the function that follows.
+  _.compose = function() {
+    var args = arguments;
+    var start = args.length - 1;
+    return function() {
+      var i = start;
+      var result = args[start].apply(this, arguments);
+      while (i--) result = args[i].call(this, result);
+      return result;
+    };
+  };
+
+  // Returns a function that will only be executed on and after the Nth call.
+  _.after = function(times, func) {
+    return function() {
+      if (--times < 1) {
+        return func.apply(this, arguments);
+      }
+    };
+  };
+
+  // Returns a function that will only be executed up to (but not including) the Nth call.
+  _.before = function(times, func) {
+    var memo;
+    return function() {
+      if (--times > 0) {
+        memo = func.apply(this, arguments);
+      }
+      if (times <= 1) func = null;
+      return memo;
+    };
+  };
 
 })

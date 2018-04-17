fb41c429743df69539c14895677286ed4996237c
fridolph <249121486@qq.com>
2018-04-17 13:23:26 +0800

    前端面试题

---
+++ 知识点积累/简单应用2/36执行顺序.js
@@ -0,0 +1,28 @@
+async function foo() {
+  console.log('1')
+  const r1 = await new Promise((resolve, reject) => {
+    console.log('2')
+    setTimeout(() => {
+      console.log('3')
+      resolve(true)
+      console.log('4')
+      reject(false)
+    }, 1000)
+  })
+  setTimeout(() => {
+    console.log('5')
+  }, 0)
+  setImmediate(() => {
+    console.log('6')
+  })
+  process.nextTick(() => {
+    console.log('7')
+  })
+  const r2 = await Promise.resolve(true)
+  console.log('8')
+}
+foo().then(() => {
+  console.log('9')
+  setTimeout(() => console.log('10'), 0)
+})
+// 1 2 3 4 8 9 7 5 10 6

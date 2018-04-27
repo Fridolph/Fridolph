6f025abd49c0699f043d8a0a69f7c1306a1416c0
fridolph <249121486@qq.com>
2018-04-27 09:52:30 +0800

    柯里化，面向对象

--- Javascript进阶/面向对象/demo/progress1.js
+++ Javascript进阶/面向对象/demo/progress1.js
@@ -2,41 +2,33 @@ class ProgressBar {
   constructor($container) {
     this.fullWidth = $container.width()
     this.$bar = null
-  }
-  
+  }  
   // 设置宽度
   setProgress(percentage) {
     this.$bar.animate({
       width: this.fullWidth * percentage + 'px'
     })
   }
-
   // 完成
   finished() {
     this.$bar.hide()
   }
-
   // 失败
   failed() {
     this.addFailedText()
   }
-
-  addFailedText() {
-    
-  }
+  addFailedText() { }
 }
 
 class ProgressBarWithNumber extends ProgressBar {
   constructor($container) {
     super($container)
   }
-
   // 多态
   setProgress(percentage) {
     // 先借助继承父类的函数
     super.setProgress(percentage)
     this.showPercentageText(percentage)
   }
-
   showPercentageText(percentage) {}
 }

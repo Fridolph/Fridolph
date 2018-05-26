e799be916baf8a8d1156daeb8bd196aa06fc78a9
fridolph <249121486@qq.com>
2018-05-26 17:04:27 +0800

    斐波那契 ~ 和 小数分号。

--- 前端面试整理/芋头的前端面试十讲/03将一个任意长的数字变成逗号分隔的形式.js
+++ 前端面试整理/芋头的前端面试十讲/03将一个任意长的数字变成逗号分隔的形式.js
@@ -1,3 +1,17 @@
 // 3.将一个任意长的数字变成逗号分割的格式
 // 1234.56 => "1,234.56" , 123456789 => "123,456,789"
 // parseToMoney(1234.56) // return "1,234.56"
+const parseToMoney = number => {
+  number += ''
+
+  if (number.indexOf('.') === -1) {
+    return number.replace(/(\d+?)(?=(\d{3})+$)/g, '$1,')
+  } else {
+    let arr = number.split('.')
+    let left = arr[0].replace(/(\d+?)(?=(\d{3})+$)/g, '$1,')
+    let right = arr[1]
+    let rArr = [left, right]
+    return rArr.join('.')
+  }
+}
+console.log(parseToMoney('12346.70291')) // return "1,234.56"
--- 算法相关/面试相关/斐波那契.js
+++ 算法相关/面试相关/斐波那契.js
@@ -1,22 +1,40 @@
-function fibonacci(n) {
-  if (n === 0) return 0
-  if (n === 1) return 1
+// function fibonacci(n) {
+//   if (n === 0) return 0
+//   if (n === 1) return 1
 
-  return fibonacci(n - 2) + fibonacci(n - 1)
-}
+//   return fibonacci(n - 2) + fibonacci(n - 1)
+// }
 
 // 改良版
-function fibonacci(n) {
-  if (n === 0) return 0
-  if (n === 1) return 1
+// function fibonacci(n) {
+//   if (n === 0) return 0
+//   if (n === 1) return 1
 
-  let a = 0
-  let b = 1
-  let t = 0
-  for (let i = 2; i <= n; i++) {
-    t = a + b
-    a = b
-    b = t
+//   let a = 0
+//   let b = 1
+//   let t = 0
+//   for (let i = 2; i <= n; i++) {
+//     t = a + b
+//     a = b
+//     b = t
+//   }
+//   return t
+// }
+
+// 最优
+// const fibonacci = (s => (
+//   f = i => s[i] || (s[i] = f(i - 1) + f(i - 2))
+// ))([0, 1, 1])
+
+const fibonnaci = n => {
+  const fib = (prev, next, count) => {
+    if (count === 0) {
+      return prev
+    } else {
+      return fib(next, prev + next, count - 1)
+    }
   }
-  return t
+  return fib(0, 1, n)
 }
+
+fibonnaci(10)
